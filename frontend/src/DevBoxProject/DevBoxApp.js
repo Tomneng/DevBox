e3814dddc2b607b56d1ccc0aae5fc7314b52e121
Tomneng <tomneng12345@gmail.com>
2024-01-23 11:33:06 +0900

    react 데브박스 만들음

---
+++ frontend/src/DevBoxProject/DevBoxApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DevBoxApp = () => {
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default DevBoxApp;
--- src/test/java/com/devbox/devbox/DevBoxApplicationTests.java
+++ src/test/java/com/devbox/devbox/DevBoxApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.devbox;
+package com.devbox.devbox;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
