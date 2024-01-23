2513b002ba020bfad6afc17e09c4d65ef4001fc3
Tomneng <tomneng12345@gmail.com>
2024-01-23 11:25:55 +0900

    init commit

---
+++ src/test/java/com/example/devbox/DevBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
