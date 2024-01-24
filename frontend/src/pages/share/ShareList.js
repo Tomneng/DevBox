d74423ac981fa24503dc0e5d539cda5b52053498
Tomneng <tomneng12345@gmail.com>
2024-01-24 18:40:54 +0900

    리액트 shareList 만드는중

--- frontend/src/pages/share/ShareList.js
+++ frontend/src/pages/share/ShareList.js
@@ -2,13 +2,32 @@ import React from "react";
 import ShareWrite from "./ShareWrite";
 import ShareDetail from "./ShareDetail";
 import ShareUpdate from "./ShareUpdate";
+import {Container, Form} from "react-bootstrap";
 
 const ShareList = () => {
-  return (
-    <div>
 
-    </div>
-  );
+    const changeValue = () => {
+
+    };
+
+    return (
+        <Container>
+            <h2>Share</h2>
+            <hr/>
+        <Form.Group>
+            <Form.Label>제목 : </Form.Label>
+            <Form.Control
+            type={"text"}
+            placeholder={"제목 입력"}
+            onChange={changeValue}
+            name={"stitle"}
+            required/>
+
+
+        </Form.Group>
+
+        </Container>
+    );
 };
 
 export default ShareList;
