3a4e0dfb2ccb7a3e6ccef8a9b436bd9452904dae
Zl존기원 <gkgk7610@naver.com>
2024-02-06 12:01:43 +0900

    ShareWrite 에서 사용 언어(slanguage) 아이콘 배치 중

--- frontend/src/DevBoxProject/DevBoxApp.js
+++ frontend/src/DevBoxProject/DevBoxApp.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React from "react";
+import ShareList from "../pages/share/ShareList";
+import ShareWrite from "../pages/share/ShareWrite";
+import ShareDetail from "../pages/share/ShareDetail";
+import ShareUpdate from "../pages/share/ShareUpdate";
+
+import {Container} from "react-bootstrap";
 import {Route, Routes} from "react-router-dom";
-import {Container} from 'react-bootstrap';
-import LoginPage from "./pages/Utils/LoginPage";
-import RegisterPage from "./pages/Utils/RegisterPage";
-import HomePage from "./pages/Utils/HomePage";
-import MyPage from "./pages/Utils/MyPage";
-import AboutPage from "./pages/AboutPage";
-import LoginContextProvider from "./contexts/LoginContextProvider";
-import AdminPage from "./pages/AdminPage";
-import ErrorLibPage from "./pages/errorLib/ErrorLibPage";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "../components/default.css"
 
 const DevBoxApp = () => {
-    return (
-        <>
-                <LoginContextProvider>
-                    <Routes>
-                        <Route path='/' Component={HomePage}></Route>
-                        <Route path='/register' Component={RegisterPage}></Route>
-                        <Route path='/login' Component={LoginPage}></Route>
-                        <Route path='/mypage' Component={MyPage}></Route>
-                        <Route path='/about' Component={AboutPage}></Route>
-                        <Route path='/admin' Component={AdminPage}></Route>
-                        <Route path='/errorLib' Component={ErrorLibPage}></Route>
-                    </Routes>
-                </LoginContextProvider>
-        </>
-    );
+		return (
+				<Container>
+
+						<Routes>
+
+								{/* Error */}
+
+
+								{/* Share  */}
+								<Route path="/list" Component={ShareList}></Route>{/*  Share 목록 */}
+								<Route path="/write" Component={ShareWrite}></Route>{/* Share 작성 */}
+								<Route path="/detail/:sid" Component={ShareDetail}></Route>{/* Share 상세 */}
+								<Route path="/update/:sid" Component={ShareUpdate}></Route>{/* Share 수정 */}
+
+
+								{/* profile */}
+
+
+						</Routes>
+				</Container>
+		);
 };
 
 export default DevBoxApp;
